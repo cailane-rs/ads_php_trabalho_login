0eb91d9f587177cf1a96a28829656e650391e0a3
Fábio <105395440+Fabio815@users.noreply.github.com>
2025-05-17 12:35:33 -0300

    Add files via upload
    Cadastro: agora verifica se o email já é cadastrado, caso seja é mostrado um aviso. E quando cadastrado vai direto para a tela de login.

--- cadastro.php
+++ cadastro.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Tela de cadastro</title>
 </head>
 <body>
 
     <?php
         $servername = "localhost";
         $username = "root";
-        $password  = "admin";
+        $password  = "root";
         $dbname = "banco";
 
         $nome = $_POST['nome'];
@@ -22,12 +22,30 @@
             $options = [
                 'cost' => 11,
             ];
+            //É aqui que vou verificar se já tem email cadastrado.
+            
+            $stmt = $conn->prepare("select id from usuarios where email=?");
+            //Aqui estou passaddo os parametros
+            $stmt->bind_param("s", $email);
+            $stmt->execute();
+            //Esse store_result meio que guarda os resultados da consulta da query.
+            $stmt->store_result();
+            //Caso tenha algum email vai retornar a linha e vai cair nesse if...
+            if ($stmt->num_rows > 0) {
+                //Morre a aplicação.
+                die("Email já cadastrado.");
+            }
+            $stmt->close();
+
             $senhaHash = password_hash($senha, PASSWORD_DEFAULT, $options);
             $stmt = $conn->prepare("insert into usuarios (nome, email, senha) values (?,?,?)");
             $stmt->bind_param("sss", $nome, $email, $senhaHash);
     
             $stmt->execute();
             $stmt->close();
+            //Direciona para a página de login
+            header("Location: login.php");
+            exit;
         } catch (Exception $e) {
             die("Erro " . $e);
         }
--- index.php
+++ index.php
@@ -11,10 +11,10 @@
         <input type="text" name="nome" required>
         <input type="email" name="email" required>
         <input id="idSenha" type="password" name="senha" required>
-        <input type="submit" name="cadastrar" required>
+        <input type="submit" name="cadastrar">
     </form>
 
-    <a href="login">Logar-se</a>
+    <a href="login.php">Logar-se</a>
     
 
     
@@ -22,7 +22,7 @@
         function validaLogin() {
             var senha = document.getElementById("idSenha");
 
-            if (senha.value.length =< 8) {
+            if (senha.value.length < 8) {
                 window.alert("A senha deve ter pelo menos 8 caracteres.");
                 return false;
             }
